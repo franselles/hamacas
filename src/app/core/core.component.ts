9b0afe211097de10b4915cec4c0145fae7b03a27
franselles <fran.selles@gmail.com>
2019-09-11 09:50:52 +0200

    A20190911-A

--- src/app/core/core.component.ts
+++ src/app/core/core.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, Renderer } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
 import { Global } from '../shared/global.services';
 
 @Component({
@@ -9,18 +9,17 @@ import { Global } from '../shared/global.services';
 export class CoreComponent implements OnInit {
   constructor(
     private el: ElementRef,
-    private renderer: Renderer,
+    private renderer: Renderer2,
     private global: Global
   ) {}
   onMenuClick() {
     // this.el.nativeElement.querySelector('.collapse navbar-collapse'); //  get the DOM
     // this.renderer.setElementClass('DOM-Element', 'css-class-you-want-to-add', false) if 3rd value is true
     // it will add the css class. 'in' class is responsible for showing the menu.
-    this.renderer.setElementClass(
-      this.el.nativeElement.querySelector('navbarNavAltMarkup'),
-      'in',
-      false
-    );
+    // this.el.nativeElement.querySelector('.collapse navbar-collapse'); //  get the DOM
+// this.renderer.setElementClass('DOM-Element', 'css-class-you-want-to-add', false) if 3rd value is true
+// it will add the css class. 'in' class is responsible for showing the menu.
+this.renderer.removeClass(this.el.nativeElement.querySelector('navbarNavAltMarkup'), 'in');
   }
 
   ngOnInit() {}
